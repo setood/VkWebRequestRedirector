e78884cd82d655aab6c32ddf9f6a75ed52590768
Vsevolod <sevik_sm@mail.ru>
2020-03-02 23:50:46 +0600

    Add project files.

---
+++ VkWebRequestRedirector/Controllers/CallbackController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using VkNet;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model;
+using VkNet.Model.Keyboard;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
+using VkWebRequestRedirector.Model;
+
+namespace VkWebRequestRedirector.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CallbackController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET api/values
+        [HttpPost]
+        public async Task<IActionResult> Callback([FromBody] object updatesObj)
+        {
+            Updates updates = JsonConvert.DeserializeObject<Updates>(updatesObj.ToString());
+            if (updates.Type == "confirmation")
+            {
+                return Ok(_configuration["Config:Confirmation"]);
+            }
+            ///////////////////////
+            var api = new VkApi();
+            api.Authorize(new ApiAuthParams {AccessToken = _configuration["Config:AccessToken"]});
+            var msg = Message.FromJson(new VkResponse(updates.Object));
+            var keyboard = new KeyboardBuilder()
+                .AddButton("Подтвердить", "btnValue", KeyboardButtonColor.Primary)
+                .SetInline(false)
+                .AddLine()
+                .AddButton("Отменить", "btnValue", KeyboardButtonColor.Primary)
+                .Build();
+            api.Messages.Send(new MessagesSendParams{ 
+                RandomId = new DateTime().Millisecond,
+                PeerId = msg.PeerId.Value,
+                Message = "redir",
+                Keyboard = keyboard
+            });
+            ////////////////
+            string url = Environment.GetEnvironmentVariable("RederectURI");
+            var httpWebRequest = (HttpWebRequest) WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = updatesObj.ToString();
+
+                streamWriter.Write(json);
+            }
+
+            var httpResponse = (HttpWebResponse) await httpWebRequest.GetResponseAsync();
+            return Ok("ok");
+        }
+    }
+}
---
+++ VkWebRequestRedirector/Model/Updates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace VkWebRequestRedirector.Model
+{
+    [Serializable]
+    public class Updates
+    {
+        /// <summary>
+        /// Тип события
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Объект, инициировавший событие
+        /// Структура объекта зависит от типа уведомления
+        /// </summary>
+        [JsonProperty("object")]
+        public JObject Object { get; set; }
+
+        /// <summary>
+        /// ID сообщества, в котором произошло событие
+        /// </summary>
+        [JsonProperty("group_id")]
+        public long GroupId { get; set; }
+
+        [JsonProperty("event_id")]
+        public string EventId { get; set; }
+    }
+}
