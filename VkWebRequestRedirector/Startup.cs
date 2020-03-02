94f74727b07b55949aec895298a7bd747c4e68a5
Vsevolod <sevik_sm@mail.ru>
2020-03-03 00:04:10 +0600

    1

--- VkWebRequestRedirector/Startup.cs
+++ VkWebRequestRedirector/Startup.cs
@@ -36,11 +36,11 @@ namespace VkWebRequestRedirector
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
